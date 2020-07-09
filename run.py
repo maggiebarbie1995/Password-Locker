e6356a65a2f9c6248ec569bf77f05b7ea8de4dde
maggiebarbie1995 <maggylangakush@gmail.com>
2020-07-09 18:56:48 +0300

    add function to create a new user

---
+++ run.py
@@ -0,0 +1,30 @@
+import random
+from user import User
+from credential import Credential
+
+# Create a user
+
+def create_user(fname, mname,password):
+
+    """
+    Function to create a new user
+    """
+
+    new_user = User(fname, mname,password)
+    return new_user
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
