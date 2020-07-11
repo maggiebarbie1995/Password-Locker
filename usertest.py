4a0d17b7f2690b3ae199d69e1498cca61c13c638
maggiebarbie1995 <maggylangakush@gmail.com>
2020-07-11 14:15:45 +0300

    add method used to find the user

--- usertest.py
+++ usertest.py
@@ -60,12 +60,51 @@ def test_save_user(self):
         self.assertEqual(len(User.users_list), 2)
 
  def test_display_user(self):
-     
+
         '''
         Test case to test if a user can see a list of all the users saved
         '''
         
         self.assertEqual( User.display_user() , User.user_list )
+
+ def test_user_exist(self):
+        
+        '''
+        Test to check if we can return a boolean if we can't find the user
+        '''
+
+     # Save the new user
+        self.new_user.save_user()
+
+        test_user = User("Leroy","smith")
+
+        test_user.save_user()
+        
+        # use contact exist method
+        user_exists = User.user_exist("Jane")
+        
+        self.assertTrue(user_exists)
+
+ def test_find_credential(self):
+
+        '''
+        Test case to test if the User module is importing from Credential module
+        '''
+
+        # Save the new user
+        self.new_user.save_user()
+
+        test_user = User("Leroy","smith")
+
+        test_user.save_user()
+
+        found_credential = User.find_credential()
+
+        self.assertEqual( found_credential, False )
+        
+ 
+
+    
 
 
 
