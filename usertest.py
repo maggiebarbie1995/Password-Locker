3c1608d8b821af0669119f71e652c616422acc7f
maggiebarbie1995 <maggylangakush@gmail.com>
2020-07-11 12:50:08 +0300

    add file changes and method used to test if we can save multiple users in the users list

--- usertest.py
+++ usertest.py
@@ -43,10 +43,21 @@ def test_save_user(self):
         test_save_user test case to test if the user object is saved into
          the users list
         """
-        
-        self.new_user.save_user_details()  # saving the new user
-        self.assertEqual(len(User.users_array), 1) 
 
+        self.new_user.save_user_details()  # saving the new user
+        self.assertEqual(len(User.users_list), 1)
+
+ def test_save_multiple_users(self):
+
+        """
+        test_save_multiple_users to check if we can save multiple users
+        to our users_array
+        """
+
+        self.new_user.save_user_details()
+        test_user = User("Test", "user", "0712345678", "test@user.com")  # new user
+        test_user.save_user_details()
+        self.assertEqual(len(User.users_list), 2)
 
 
 
